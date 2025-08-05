3391192fe3eb4542914482363d6ac6b27c257efd
hyunjoo <khjoo1203@gmail.com>
2025-08-05 21:00:43 +0900

    이미지 업로드하기1

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { Form, Modal, Button, Row, Col, Alert } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import CloudinaryUploadWidget from "../../../utils/CloudinaryUploadWidget";
-import { CATEGORY, STATUS, SIZE } from "../../../constants/product.constants";
-import "../style/adminProduct.style.css";
+import React, { useState, useEffect } from 'react';
+import { Form, Modal, Button, Row, Col, Alert } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import CloudinaryUploadWidget from '../../../utils/CloudinaryUploadWidget';
+import { CATEGORY, STATUS, SIZE } from '../../../constants/product.constants';
+import '../style/adminProduct.style.css';
 import {
   clearError,
   createProduct,
   editProduct,
-} from "../../../features/product/productSlice";
+} from '../../../features/product/productSlice';
+import { image } from '@cloudinary/url-gen/qualifiers/source';
 
 const InitialFormData = {
-  name: "",
-  sku: "",
+  name: '',
+  sku: '',
   stock: {},
-  image: "",
-  description: "",
+  image: '',
+  description: '',
   category: [],
-  status: "active",
+  status: 'active',
   price: 0,
 };
 
@@ -26,11 +27,12 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     (state) => state.product
   );
   const [formData, setFormData] = useState(
-    mode === "new" ? { ...InitialFormData } : selectedProduct
+    mode === 'new' ? { ...InitialFormData } : selectedProduct
   );
   const [stock, setStock] = useState([]);
   const dispatch = useDispatch();
   const [stockError, setStockError] = useState(false);
+  console.log('stock', stock);
 
   useEffect(() => {
     if (success) setShowDialog(false);
@@ -41,7 +43,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
       dispatch(clearError());
     }
     if (showDialog) {
-      if (mode === "edit") {
+      if (mode === 'edit') {
         setFormData(selectedProduct);
         // 객체형태로 온 stock을  다시 배열로 세팅해주기
         const sizeArray = Object.keys(selectedProduct.stock).map((size) => [
@@ -66,7 +68,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     //재고를 입력했는지 확인, 아니면 에러
     // 재고를 배열에서 객체로 바꿔주기
     // [['M',2]] 에서 {M:2}로
-    if (mode === "new") {
+    if (mode === 'new') {
       //새 상품 만들기
     } else {
       // 상품 수정하기
@@ -75,22 +77,33 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const handleChange = (event) => {
     //form에 데이터 넣어주기
+    const { id, value } = event.target;
+    setFormData({ ...formData, [id]: value });
   };
 
   const addStock = () => {
     //재고타입 추가시 배열에 새 배열 추가
+    setStock([...stock, []]);
   };
 
   const deleteStock = (idx) => {
     //재고 삭제하기
+    const newStock = stock.filter((item, index) => index !== idx);
+    setStock(newStock);
   };
 
   const handleSizeChange = (value, index) => {
     //  재고 사이즈 변환하기
+    const newStock = [...stock];
+    newStock[index][0] = value;
+    setStock(newStock);
   };
 
   const handleStockChange = (value, index) => {
     //재고 수량 변환하기
+    const newStock = [...stock];
+    newStock[index][1] = value;
+    setStock(newStock);
   };
 
   const onHandleCategory = (event) => {
@@ -112,53 +125,54 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const uploadImage = (url) => {
     //이미지 업로드
+    setFormData({ ...formData, image: url });
   };
 
   return (
     <Modal show={showDialog} onHide={handleClose}>
       <Modal.Header closeButton>
-        {mode === "new" ? (
+        {mode === 'new' ? (
           <Modal.Title>Create New Product</Modal.Title>
         ) : (
           <Modal.Title>Edit Product</Modal.Title>
         )}
       </Modal.Header>
       {error && (
-        <div className="error-message">
-          <Alert variant="danger">{error}</Alert>
+        <div className='error-message'>
+          <Alert variant='danger'>{error}</Alert>
         </div>
       )}
-      <Form className="form-container" onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="sku">
+      <Form className='form-container' onSubmit={handleSubmit}>
+        <Row className='mb-3'>
+          <Form.Group as={Col} controlId='sku'>
             <Form.Label>Sku</Form.Label>
             <Form.Control
               onChange={handleChange}
-              type="string"
-              placeholder="Enter Sku"
+              type='string'
+              placeholder='Enter Sku'
               required
               value={formData.sku}
             />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="name">
+          <Form.Group as={Col} controlId='name'>
             <Form.Label>Name</Form.Label>
             <Form.Control
               onChange={handleChange}
-              type="string"
-              placeholder="Name"
+              type='string'
+              placeholder='Name'
               required
               value={formData.name}
             />
           </Form.Group>
         </Row>
 
-        <Form.Group className="mb-3" controlId="description">
+        <Form.Group className='mb-3' controlId='description'>
           <Form.Label>Description</Form.Label>
           <Form.Control
-            type="string"
-            placeholder="Description"
-            as="textarea"
+            type='string'
+            placeholder='Description'
+            as='textarea'
             onChange={handleChange}
             rows={3}
             value={formData.description}
@@ -166,15 +180,15 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="stock">
-          <Form.Label className="mr-1">Stock</Form.Label>
+        <Form.Group className='mb-3' controlId='stock'>
+          <Form.Label className='mr-1'>Stock</Form.Label>
           {stockError && (
-            <span className="error-message">재고를 추가해주세요</span>
+            <span className='error-message'>재고를 추가해주세요</span>
           )}
-          <Button size="sm" onClick={addStock}>
+          <Button size='sm' onClick={addStock}>
             Add +
           </Button>
-          <div className="mt-2">
+          <div className='mt-2'>
             {stock.map((item, index) => (
               <Row key={index}>
                 <Col sm={4}>
@@ -183,14 +197,13 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
                       handleSizeChange(event.target.value, index)
                     }
                     required
-                    defaultValue={item[0] ? item[0].toLowerCase() : ""}
+                    defaultValue={item[0] ? item[0].toLowerCase() : ''}
                   >
-                    <option value="" disabled selected hidden>
+                    <option value='' disabled hidden>
                       Please Choose...
                     </option>
                     {SIZE.map((item, index) => (
                       <option
-                        inValid={true}
                         value={item.toLowerCase()}
                         disabled={stock.some(
                           (size) => size[0] === item.toLowerCase()
@@ -207,16 +220,16 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
                     onChange={(event) =>
                       handleStockChange(event.target.value, index)
                     }
-                    type="number"
-                    placeholder="number of stock"
+                    type='number'
+                    placeholder='number of stock'
                     value={item[1]}
                     required
                   />
                 </Col>
                 <Col sm={2}>
                   <Button
-                    variant="danger"
-                    size="sm"
+                    variant='danger'
+                    size='sm'
                     onClick={() => deleteStock(index)}
                   >
                     -
@@ -227,34 +240,34 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
           </div>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="Image" required>
+        <Form.Group className='mb-3' controlId='Image' required>
           <Form.Label>Image</Form.Label>
           <CloudinaryUploadWidget uploadImage={uploadImage} />
 
           <img
-            id="uploadedimage"
+            id='uploadedimage'
             src={formData.image}
-            className="upload-image mt-2"
-            alt="uploadedimage"
-          ></img>
+            className='upload-image mt-2'
+            alt='uploadedimage'
+          />
         </Form.Group>
 
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="price">
+        <Row className='mb-3'>
+          <Form.Group as={Col} controlId='price'>
             <Form.Label>Price</Form.Label>
             <Form.Control
               value={formData.price}
               required
               onChange={handleChange}
-              type="number"
-              placeholder="0"
+              type='number'
+              placeholder='0'
             />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="category">
+          <Form.Group as={Col} controlId='category'>
             <Form.Label>Category</Form.Label>
             <Form.Control
-              as="select"
+              as='select'
               multiple
               onChange={onHandleCategory}
               value={formData.category}
@@ -268,7 +281,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             </Form.Control>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="status">
+          <Form.Group as={Col} controlId='status'>
             <Form.Label>Status</Form.Label>
             <Form.Select
               value={formData.status}
@@ -283,12 +296,12 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             </Form.Select>
           </Form.Group>
         </Row>
-        {mode === "new" ? (
-          <Button variant="primary" type="submit">
+        {mode === 'new' ? (
+          <Button variant='primary' type='submit'>
             Submit
           </Button>
         ) : (
-          <Button variant="primary" type="submit">
+          <Button variant='primary' type='submit'>
             Edit
           </Button>
         )}
--- src/utils/CloudinaryUploadWidget.js
+++ src/utils/CloudinaryUploadWidget.js
@@ -1,40 +1,60 @@
-import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-import "../App.css";
-import "../common/style/common.style.css";
+import React, { Component, createRef } from 'react';
+import { Button } from 'react-bootstrap';
+import '../App.css';
+import '../common/style/common.style.css';
 
 const CLOUDNAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 const UPLOADPRESET = process.env.REACT_APP_CLOUDINARY_PRESET;
 
 class CloudinaryUploadWidget extends Component {
-  componentDidMount() {
-    var myWidget = window.cloudinary.createUploadWidget(
-      {
-        cloudName: CLOUDNAME,
-        uploadPreset: UPLOADPRESET,
-      },
-      (error, result) => {
-        if (!error && result && result.event === "success") {
-          console.log("Done! Here is the image info: ", result.info);
-          document
-            .getElementById("uploadedimage")
-            .setAttribute("src", result.info.secure_url);
-          this.props.uploadImage(result.info.secure_url);
-        }
-      } //https://cloudinary.com/documentation/react_image_and_video_upload
-    );
-    document.getElementById("upload_widget").addEventListener(
-      "click",
-      function () {
-        myWidget.open();
-      },
-      false
-    );
+  constructor(props) {
+    super(props);
+    this.buttonRef = createRef();
+    this.myWidget = null;
   }
+
+  componentDidMount() {
+    const tryCreateWidget = () => {
+      if (window.cloudinary && window.cloudinary.createUploadWidget) {
+        this.myWidget = window.cloudinary.createUploadWidget(
+          {
+            cloudName: CLOUDNAME,
+            uploadPreset: UPLOADPRESET,
+          },
+          (error, result) => {
+            if (!error && result && result.event === 'success') {
+              document
+                .getElementById('uploadedimage')
+                .setAttribute('src', result.info.secure_url);
+              this.props.uploadImage(result.info.secure_url);
+            }
+          }
+        );
+        if (this.buttonRef.current) {
+          this.buttonRef.current.addEventListener('click', this.openWidget);
+        }
+      } else {
+        setTimeout(tryCreateWidget, 100);
+      }
+    };
+    tryCreateWidget();
+  }
+
+  componentWillUnmount() {
+    if (this.buttonRef.current) {
+      this.buttonRef.current.removeEventListener('click', this.openWidget);
+    }
+  }
+
+  openWidget = () => {
+    if (this.myWidget) {
+      this.myWidget.open();
+    }
+  };
 
   render() {
     return (
-      <Button id="upload_widget" size="sm" className="ml-2">
+      <Button ref={this.buttonRef} size='sm' className='ml-2'>
         Upload Image +
       </Button>
     );
